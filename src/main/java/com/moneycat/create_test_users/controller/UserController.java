7233c5220385c12b24f77abdcd0f4b22ad657de6
jack <evzhurilo@gmail.com>
2024-02-25 17:01:42 -0400

    fourth commit

--- src/main/java/com/moneycat/create_test_users/controller/UserController.java
+++ src/main/java/com/moneycat/create_test_users/controller/UserController.java
@@ -10,42 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/users")
+
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserController {
-
     private final UserService service;
-
-
-//    @GetMapping("/main")
-//    public ModelAndView showCreateForm(Model model) {
-//        model.addAttribute("account", new Account());
-//        return new ModelAndView("create_user");
-//    }
-
     @GetMapping("/main")
     public String showCreateForm(Model model) {
         User newAccount = new User();
         model.addAttribute("account", newAccount);
         return "create_user";
     }
-
     @PostMapping("/create")
     public String createUser(@ModelAttribute("name") String name, @ModelAttribute("email") String email, @ModelAttribute("password") String password) {
         service.createUser(new UserCreateRequest(name, email, password));
         return "redirect:/main";
     }
-
-//    @PostMapping("/create")
-//    public void createUser(@ModelAttribute String username, @ModelAttribute String email, @ModelAttribute String password) {
-//        Account newAcccount = Account.builder()
-//                .username(username)
-//                .email(email)
-//                .password(password)
-//                .build();
-//        service.createAccount(newAcccount);
-//    }
-
 
 }
 
